10e059121bc04658c933bbb0485dc181c4daa666
likeai <like.aiquan@qq.com>
2025-10-30 21:53:24 +0800

    surge script test

--- Surge/script/mock.js
+++ Surge/script/mock.js
@@ -2,4 +2,9 @@
 ; (async () => {
     let headers = $response.headers;
     headers["X-Surge-Mock-Type"] = "surge modify reposne header"
+
+    let content = $response.content
+    console.log(content)
+
+    $done({ headers });
 })()
